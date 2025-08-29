a72eaa6e7da2226fc78bd817e344db030c1a70d0
geekyneha <nehatualea@gmail.com>
2025-08-29 17:49:26 +0530

    What will be the output soln

---
+++ day04/Q4_Output.java
@@ -0,0 +1,13 @@
+package day04;
+
+public class Q4_Output {
+  public static void main(String args[]) {
+    int a=63,b=36;
+    boolean x= (a<b) ? true : false;
+    int y= (a>b)?a:b;
+    
+    
+    System.out.println(x); // false
+    System.out.println(y); // 63
+  }
+}
