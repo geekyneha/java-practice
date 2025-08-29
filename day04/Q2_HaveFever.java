e9b22212bb1ac85a50e4021b455da78cfce735ca
geekyneha <nehatualea@gmail.com>
2025-08-29 17:49:03 +0530

    Check Have Fever or Not

---
+++ day04/Q2_HaveFever.java
@@ -0,0 +1,18 @@
+package day04;
+
+public class Q2_HaveFever {
+
+    public static void main(String args[]){
+
+        double  temprature = 100;
+
+        if(temprature > 100){
+            System.out.println("you have a Fever");
+        }
+        else{
+            System.out.println("you don't have  a Fever");
+        }
+
+    }
+    
+}
