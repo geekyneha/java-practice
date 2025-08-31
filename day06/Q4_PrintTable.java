ff33f7b5c18a8158c0f499aa6e5570faa91a329c
geekyneha <nehatualea@gmail.com>
2025-08-31 20:37:09 +0530

    Print table of given number

---
+++ day06/Q4_PrintTable.java
@@ -0,0 +1,19 @@
+package day06;
+import java.util.*;
+
+public class Q4_PrintTable {
+    
+    public static void main(String args[]){
+        
+        Scanner sc = new Scanner(System.in);
+         
+        System.out.println("Enter A Number");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++){
+            System.out.println(n*i);
+        }
+
+        sc.close();
+    }
+}
