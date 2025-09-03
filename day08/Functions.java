93d2c6693d90c0fc91526b6c024b87bbd17bb344
geekyneha <nehatualea@gmail.com>
2025-09-03 22:05:52 +0530

    Functions

---
+++ day08/Functions.java
@@ -0,0 +1,16 @@
+package day08;
+
+public class Functions {
+
+    static void myMethod(){
+        System.out.println("My method");
+            
+        }
+    public static void main(String[] args) {
+
+       
+        myMethod();
+        
+    }
+    
+}
