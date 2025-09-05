21fe3d1da548c808567a0d93347fc9a4559e918c
geekyneha <nehatualea@gmail.com>
2025-09-05 18:59:50 +0530

    Decimal tp binary conversion

--- day08/NumberSys.java
+++ day08/NumberSys.java
@@ -2,6 +2,7 @@ package day08;
 
 public class NumberSys {
     public static void bintoDec(int n){
+        int num = n;
 
         int pow = 0;
         int dec= 0;
@@ -14,12 +15,33 @@ public class NumberSys {
             pow++;
             n = n / 10;
         }
-       System.out.println("Decimal: " 
+       System.out.println(num + " in Decimal: " 
        + dec);
+    }
+
+    public static void dectoBin (int n){
+        
+        int num = n;
+        int pow =0;
+        int binNum = 0;
+
+        while(n > 0){
+
+             int rem = n % 2;
+
+             binNum  = binNum + (rem * (int)Math.pow(10,pow));
+
+             pow++;
+             n = n / 2;
+
+        }
+
+        System.out.println(num +" in Binary: " + binNum);
     }
 
     public static void main(String[] args) {
         bintoDec(101);
+        dectoBin(7);
         
     }
     
