b707f973052029cc03a35438059ce59cdbc4126a
geekyneha <nehatualea@gmail.com>
2025-09-11 20:46:41 +0530

    Day 12: Bubble Sort

---
+++ day12/BubbleSort.java
@@ -0,0 +1,33 @@
+package day12;
+
+public class BubbleSort {
+
+    public static void bubblesort(int arr[]){
+        
+        int n = arr.length;
+       
+        for(int i = 0 ; i < arr.length ; i++){
+            
+            for(int j = 0 ; j < arr.length-1-i ; j++)
+            
+            if(arr[j] > arr[j+1]){
+                int temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[] = {2,3,5,1,2,7,11,7};
+
+        bubblesort(arr);
+
+        for(int i = 0 ; i< arr.length ; i++){
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+    
+}
