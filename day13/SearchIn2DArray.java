eb205994654740bec5cd8926910984eaa92bc785
geekyneha <nehatualea@gmail.com>
2025-09-15 20:18:34 +0530

    Search , smallest , largest in 2D array

--- day13/SearchIn2DArray.java
+++ day13/SearchIn2DArray.java
@@ -14,12 +14,55 @@ public class SearchIn2DArray {
         }
       }
 
+
+      
+      }
+
+      public static int findLargest(int [][]arr){
+
+
+        int largest = Integer.MIN_VALUE;
+        
+       
+      for(int i = 0 ; i < arr.length ; i++){
+        for(int j = 0; j < arr[0].length; j++){
+
+            if(arr[i][j] > largest){
+                largest = arr[i][j];
+            }     
+                
+            }
+        }
+       
+        return largest;
     }
 
+     public static int findSmallest(int [][]arr){
+
+
+        int smallest = Integer.MAX_VALUE;
+        
+       
+      for(int i = 0 ; i < arr.length ; i++){
+        for(int j = 0; j < arr[0].length; j++){
+
+            if(arr[i][j] < smallest){
+                smallest = arr[i][j];
+            }     
+                
+            }
+        }
+       
+        return smallest;
+    }
+
+
     public static void main(String[] args) {
-        int arr[][] = {{1,2,3},{1,2,5},{4,5,6}};
+        int arr[][] = {{0,2,3},{1,7,5},{4,5,6}};
           
         search(arr,6);
+       System.out.println( findLargest(arr));
+       System.out.println( findSmallest(arr));
 
     }
     
