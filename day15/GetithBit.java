4f4542d4435ffa24ea41a34b49537e975bf91169
geekyneha <nehatualea@gmail.com>
2025-09-19 23:29:51 +0530

    Get ith Bit

---
+++ day15/GetithBit.java
@@ -0,0 +1,20 @@
+package day15;
+
+public class GetithBit {
+    
+    public static int getIthBit(int n, int i){
+        int bitMask = 1<<i;
+
+        if((n & bitMask) == 0){
+            return 0;
+        } else {
+            return 1;
+        }
+    }
+
+    public static void main(String[] args){
+
+        System.out.println(getIthBit(10, 2));
+
+    }
+}
