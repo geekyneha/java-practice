dd64ad78a0764cf236ba64833b0358e1e335a7e8
geekyneha <nehatualea@gmail.com>
2025-09-20 20:17:35 +0530

    Clear ith Bit

---
+++ day15/ClearithBit.java
@@ -0,0 +1,17 @@
+package day15;
+
+public class ClearithBit {
+
+
+    public static int clearIthBit(int n, int i) {
+        int bitMask = ~(1 << i);
+        return n & bitMask;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(clearIthBit(10, 1));
+        
+    }
+    
+}
