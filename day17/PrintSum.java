0490317767ea3d7484625964f0c3b8397042c660
geekyneha <nehatualea@gmail.com>
2025-09-23 22:49:25 +0530

    Recursion: print sum of n natural Numbers

---
+++ day17/PrintSum.java
@@ -0,0 +1,18 @@
+package day17;
+
+public class PrintSum {
+
+
+    public static int sum(int n){
+
+        if(n==1) return 1;
+
+        return n + sum(n-1);
+    }
+    public static void main(String[] args) {
+
+        System.out.println(sum(10));
+        
+    }
+    
+}
