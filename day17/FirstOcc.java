7b4a7af175d0a31b5585b77ee36f1ad2fa92ab54
geekyneha <nehatualea@gmail.com>
2025-09-24 00:35:00 +0530

    Recursion: First Occurence

--- day17/FirstOcc.java
+++ day17/FirstOcc.java
@@ -4,7 +4,7 @@ public class FirstOcc {
     
     public static int firstOcc(int arr[], int key , int i){
         
-        if(i == arr.length-1) return -1;
+        if(i == arr.length) return -1;
 
         if(arr[i] == key) return i;
 
@@ -13,8 +13,8 @@ public class FirstOcc {
     }
     public static void main(String[] args) {
 
-        int arr[]={1,2,45,6,79,9,8,6,45};
-        System.out.println(firstOcc(arr, 7, 0));
+        int arr[]={1,2,6,79,9,8,6,45};
+        System.out.println(firstOcc(arr, 45, 0));
         
     }
     
