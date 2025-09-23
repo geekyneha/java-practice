954d679b26b4c6367260f63b5437b4c40104ddc5
geekyneha <nehatualea@gmail.com>
2025-09-24 00:45:44 +0530

    Recursion: Last Occurence

--- day17/LastOcc.java
+++ day17/LastOcc.java
@@ -4,11 +4,14 @@ public class LastOcc {
 
 
     public static int lastOcc(int arr[], int key,int i){
-        if(i<0) return -1;
+        if(i==arr.length) return -1;
 
-        if(arr[i]==key) return i;
 
-        return lastOcc(arr,key,i-1);
+        int isFound = lastOcc(arr,key,i+1);
+
+        if(isFound == -1 && arr[i]==key) return i;
+
+        return isFound;
     }
 
 
@@ -16,8 +19,8 @@ public class LastOcc {
     public static void main(String[] args) {
 
 
-        int arr[] = {1,3,4,6,7,3,89,9};
-        System.out.println(lastOcc(arr, 1, arr.length-1));
+        int arr[] = {1,3,4,6,7,3,89,9,4};
+        System.out.println(lastOcc(arr, 4, 0));
         
     }
     
