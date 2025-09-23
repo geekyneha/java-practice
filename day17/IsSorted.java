6914a226bde0fb3d6b701004a5ef0ac0bcd79e73
geekyneha <nehatualea@gmail.com>
2025-09-24 00:00:03 +0530

    Recursion: Check if array is sorted

---
+++ day17/IsSorted.java
@@ -0,0 +1,19 @@
+package day17;
+
+public class IsSorted {
+
+    public static boolean isSorted(int arr[],int i){
+
+        if(i==arr.length-1) return true;
+
+        if(arr[i] > arr[i+1]) return false;
+        return isSorted(arr,i+1);
+
+    }
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
