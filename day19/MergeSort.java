c0e0a161114e29c48ea94e98beb0bbd216f368bd
geekyneha <nehatualea@gmail.com>
2025-09-26 00:05:11 +0530

    Merge Sort: devide and Conquer

--- day19/MergeSort.java
+++ day19/MergeSort.java
@@ -13,42 +13,48 @@ public class MergeSort {
     
     public static void merge(int arr[], int si, int mid, int ei){
 
-        int n1 = mid - si + 1;
-        int n2 = ei - mid;
+       
+        int temp[]= new int[ei-si + 1];
+        int i = si; // starting index of left subarray
+        int j=mid+1; // starting index of right subarray
+        int k=0; // iterator index of temp array
+        ;
 
-        int left[] = new int[n1];
-        int right[] = new int[n2];
-
-        for(int i=0; i<n1; i++){
-            left[i] = arr[si + i];
-        }
-        for(int j=0; j<n2; j++){
-            right[j] = arr[mid + 1 + j];
-        }
-
-        int i = 0, j = 0, k = si;
-
-        while(i < n1 && j < n2){
-            if(left[i] <= right[j]){
-                arr[k++] = left[i++];
-            } else {
-                arr[k++] = right[j++];
+        while(i<=mid && j<=ei){
+            if(arr[i]<=arr[j]){
+                temp[k] = arr[i];
+                i++;
+            }else{
+                temp[k] = arr[j];
+                j++;
             }
+            k++;
+
+
         }
 
-        while(i < n1){
-            arr[k++] = left[i++];
+        while(i<=mid){
+            temp[k]=arr[i];
+            i++;
+            k++;
         }
 
-        while(j < n2){
-            arr[k++] = right[j++];
+        while(j<=ei){
+            temp[k]=arr[j];
+            j++;
+            k++;    
         }
-
-        printArr(arr);
+        // copy temp to original array
+        for(k=0, i=si; k<temp.length; k++, i++){
+            arr[i]=temp[k];
+        }
     }
     public static void mergeSort(int arr[], int si, int ei){
 
         int mid = si+(ei - si) / 2;
+        if(si >= ei){
+            return;
+        }
        mergeSort(arr, si, mid);
        mergeSort(arr, mid+1,ei);
        merge(arr,si,mid,ei);
@@ -57,6 +63,7 @@ public class MergeSort {
     public static void main(String[] args) {
         int arr[]={38, 27, 43, 3, 9, 82, 10};
         mergeSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
     
 }
