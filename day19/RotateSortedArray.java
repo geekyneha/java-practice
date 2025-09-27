6c9d20d5af46a8c3db498f8f3436bec03a7e2a7d
geekyneha <nehatualea@gmail.com>
2025-09-27 23:40:37 +0530

    Sorted rotated

--- day19/RotateSortedArray.java
+++ day19/RotateSortedArray.java
@@ -1,5 +1,25 @@
 package day19;
 
 public class RotateSortedArray {
+    public static int findPivot(int arr[]){
+        int si = 0;
+        int ei = arr.length - 1;
+
+        while(si < ei){
+            int mid = si + (ei - si) / 2;
+
+            if(arr[mid] >= arr[0]){
+                si = mid + 1;
+            } else {
+                ei = mid;
+            }
+        }
+        return si;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {4,5,6,7,0,1,2};
+        System.out.println("Pivot is at index: " + findPivot(arr));
+    }
     
 }
