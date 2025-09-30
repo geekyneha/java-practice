575b302f84194c6a2cbecfd1b5b480718af167ca
geekyneha <nehatualea@gmail.com>
2025-09-30 19:26:21 +0530

    Search In Sorted and Rotated

---
+++ day19/SearchinSortedandRotated.java
@@ -0,0 +1,44 @@
+package day19;
+
+public class SearchinSortedandRotated {
+    // using recursion
+
+    public static int search(int arr[], int target, int si, int ei){
+        if(si > ei){
+            return -1;
+        }
+
+        int mid = si + (ei - si) / 2;
+
+        if(arr[mid] == target){
+            return mid;
+        }
+
+        // left part is sorted
+        if(arr[si] <= arr[mid]){
+            if(target >= arr[si] && target <= arr[mid]){
+                return search(arr, target, si, mid - 1);
+            } else {
+                return search(arr, target, mid + 1, ei);
+            }
+        } else { // right part is sorted
+            if(target >= arr[mid] && target <= arr[ei]){
+                return search(arr, target, mid + 1, ei);
+            } else {
+                return search(arr, target, si, mid - 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {4,5,6,7,0,1,2};
+        int target = 0;
+        int result = search(arr, target, 0, arr.length - 1);
+        if(result != -1){
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
+    
+}
