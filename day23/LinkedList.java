305810489b5925f5e0aa8ec802680df89c6da43b
geekyneha <nehatualea@gmail.com>
2025-10-05 20:23:50 +0530

    Linked List

--- day23/LinkedList.java
+++ day23/LinkedList.java
@@ -1,5 +1,35 @@
 package day23;
 
 public class LinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
+    static void printList(Node head) {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        printList(head);
+    }
     
 }
