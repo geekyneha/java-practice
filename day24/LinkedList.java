a4888351ac6433cfa8cbc43d30d407c070760b74
geekyneha <nehatualea@gmail.com>
2025-10-07 23:41:14 +0530

    Linked List

---
+++ day24/LinkedList.java
@@ -0,0 +1,49 @@
+package day24;
+
+public class LinkedList {
+
+    Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int d) {
+            data = d;
+            next = null;
+        }
+    }
+
+    public void insert(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node current = head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.display(); // Output: 10 -> 20 -> 30 -> null
+    }
+
+
+    
+}
