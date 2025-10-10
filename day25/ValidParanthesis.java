87da7b2b396598c42a71920036f3de566fdadf20
geekyneha <nehatualea@gmail.com>
2025-10-10 21:10:36 +0530

    Stack

---
+++ day25/ValidParanthesis.java
@@ -0,0 +1,29 @@
+package day25;
+
+public class ValidParanthesis {
+    public static boolean isValid(String s) {
+        java.util.Stack<Character> stack = new java.util.Stack<>();
+        
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                    (ch == '}' && top != '{') ||
+                    (ch == ']' && top != '[')) {
+                    return false;
+                }
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        String s = "{[()]}";
+        System.out.println(isValid(s)); // Output: true
+    }
+    
+}
