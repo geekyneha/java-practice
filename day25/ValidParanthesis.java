29594be0cc065792e150247ab2819a70f452f0d4
geekyneha <nehatualea@gmail.com>
2025-10-20 22:47:37 +0530

    Valid parenthesis

--- day25/ValidParanthesis.java
+++ day25/ValidParanthesis.java
@@ -1,14 +1,26 @@
-package day25;
+package day25;  
+import java.util.Stack;
 
 public class ValidParanthesis {
     public static boolean isValid(String s) {
-        java.util.Stack<Character> stack = new java.util.Stack<>();
+       Stack<Character> stack = new Stack<>();
         
-        for (char ch : s.toCharArray()) {
+        for (int i=0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            //opening bracket
+            // push it to stack
             if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             } else {
+
+                // closing bracket
+                //if no opening bracket is there in stack
+            
                 if (stack.isEmpty()) return false;
+
+                //pop the top element and check if it matches
                 char top = stack.pop();
                 if ((ch == ')' && top != '(') ||
                     (ch == '}' && top != '{') ||
