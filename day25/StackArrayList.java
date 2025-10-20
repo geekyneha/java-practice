230cb63b5259677b64d649b7af7fb3f04c65a0c9
geekyneha <nehatualea@gmail.com>
2025-10-20 22:03:08 +0530

    Stack using ArrayList

---
+++ day25/StackArrayList.java
@@ -0,0 +1,64 @@
+package day25;
+import java.util.ArrayList;
+
+public class StackArrayList {
+
+    static class Stack{
+
+       static ArrayList<Integer> list = new ArrayList<>();
+       
+      public static boolean isEmpty(){
+        return list.size()==0;
+      } 
+      public static void push(int x){
+
+      list.add(x);
+
+    } 
+
+    public static int pop(){
+
+        if(isEmpty()){
+            return -1;
+        }
+
+        int top = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return top;
+
+    }
+
+    public static int peek(){
+
+        if(isEmpty()){
+            return -1;
+        }
+        return list.get(list.size()-1);
+    }
+      
+    }
+
+    public static void main(String args[]){
+        Stack s= new Stack();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+     
+
+        
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+
+        System.out.println(s.isEmpty());
+
+
+
+
+
+       
+    }
+    
+}
