52ae4f8c6f8cf858a473a0891be02ca4b8e3013e
geekyneha <nehatualea@gmail.com>
2025-10-23 23:32:27 +0530

    Stock span

--- day25/Stock.java
+++ day25/Stock.java
@@ -1,5 +1,29 @@
 package day25;
 
 public class Stock {
+
+    // stock span problem;
+
+    public static void stockSpan(int [] prices, int [] span){
+      for(int i=0; i<prices.length; i++){
+        
+      }
+
+    }
+
+
+
+   
+
+
+    public static void main(String[] args){
+
+        int[] prices = {100,80,60,70,60,75,85};
+        int n = prices.length;
+        int [] span = new int[n];
+
+
+
+    }
     
 }
