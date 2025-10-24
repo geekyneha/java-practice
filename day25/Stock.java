abd7e7bd416be41e5236fc53a0df6121139ab628
geekyneha <nehatualea@gmail.com>
2025-10-24 21:02:15 +0530

    stock Span

--- day25/Stock.java
+++ day25/Stock.java
@@ -6,6 +6,10 @@ public class Stock {
 
     public static void stockSpan(int [] prices, int [] span){
       for(int i=0; i<prices.length; i++){
+        span[i] = 1; // Initialize span for each day
+        for(int j=i-1; j>=0 && prices[j] <= prices[i]; j--){ // Check previous days
+            span[i]++;
+        }
         
       }
 
@@ -21,6 +25,7 @@ public class Stock {
         int[] prices = {100,80,60,70,60,75,85};
         int n = prices.length;
         int [] span = new int[n];
+
 
 
 
