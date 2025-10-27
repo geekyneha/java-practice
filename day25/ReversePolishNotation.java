690d86c9272bc3219e3e74e2de569e68e45f4ccf
geekyneha <nehatualea@gmail.com>
2025-10-27 22:18:14 +0530

    Reverse Polish Notation

---
+++ day25/ReversePolishNotation.java
@@ -0,0 +1,39 @@
+package day25;
+
+public class ReversePolishNotation {
+    public int evalRPN(String[] tokens) {
+        java.util.Stack<Integer> stack = new java.util.Stack<>();
+
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                int result = applyOperator(a, b, token);
+                stack.push(result);
+            } else {
+                stack.push(Integer.parseInt(token));
+            }
+        }
+
+        return stack.pop();
+    }
+
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/");
+    }
+
+    private int applyOperator(int a, int b, String operator) {
+        switch (operator) {
+            case "+":
+                return a + b;
+            case "-":
+                return a - b;
+            case "*":
+                return a * b;
+            case "/":
+                return a / b;
+            default:
+                throw new IllegalArgumentException("Invalid operator: " + operator);
+        }
+    }
+}
