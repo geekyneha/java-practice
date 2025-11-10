6686c4d5b9f5a8a007b4050916993cab0e507952
geekyneha <nehatualea@gmail.com>
2025-11-10 19:24:44 +0530

    Next greater element

--- day25/NextGreaterII.java
+++ day25/NextGreaterII.java
@@ -1,5 +1,43 @@
 package day25;
+import java.util.Stack;
 
 public class NextGreaterII {
+
+    public static void main(String args[]){
+
+        int arr[] = {6,8,0,1,3};
+        int n = arr.length;
+
+        Stack<Integer> s = new Stack<>();
+
+        int nxtG[] = new int[n];
+
+        for(int i= n-1; i>=0; i--){
+            //pop smaller elements
+            while(!s.isEmpty() && s.peek() <= arr[i]){
+                s.pop();
+            }
+
+            //check if stack is empty
+            if(s.isEmpty()){
+                nxtG[i]=-1;
+            }else{
+                nxtG[i]=s.peek();
+            }
+            //push current element
+            s.push(arr[i]);
+
+        }
+
+        System.out.println("Next Greater elements are: "+ nxtG);
+
+        for(int i=0; i<n; i++){
+            System.out.print(nxtG[i] + " ");
+
+        }
+
+
+
+    }
     
 }
